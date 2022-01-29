2f12f4541d1cf1ef5f06b9abdf560f2877dae928
GhanGhan <ghanlawal@gmail.com>
2022-01-28 21:35:33 -0500

    Basic two wheel motor control working

--- wheel_control/wheel_control.ino
+++ wheel_control/wheel_control.ino
@@ -1,25 +1,41 @@
 #define enA 9
+#define enB 10
 #define in1 7
 #define in2 6
+
+#define in3 5
+#define in4 4
 
 int rotDirection = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
   // Set initial rotation direction
-  digitalWrite(in1, LOW);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int speed = 100;
-  analogWrite(enA, speed);
+  int speedA = 255;
+  int speedB = 255;
+  analogWrite(enA, speedA);
+  analogWrite(enB, speedB);
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
   delay(500);
   
 
