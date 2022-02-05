cd47f0965a6c39be64412a3871c2840a3e6809e0
GhanGhan <ghanlawal@gmail.com>
2022-02-04 19:35:31 -0500

    no longer need Cool_Term_Min_Input_Byte_Size, full control over motor speed and direction of both wheels as a pair

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,20 @@ const byte in2 = 6;
 const byte in3 = 5;//Inputs for Motor B
 const byte in4 = 4;
 
-
-const byte encA_A = 2;//A Pin -> the interrupt pin 0
-const byte encA_B = 8;//B Pin
-byte encoder0PinALast;
-int duration = 0;//the number of the pulses
+//Encoder pins for Motor A
+const byte encA1 = 2;//A Pin -> the interrupt pin 0
+const byte encA2 = 8;//B Pin
+byte previous_A1;
+volatile int duration = 0;//the number of the pulses
 float rpm = 0;
 boolean Direction;//the rotation direction
 
 
-const int Pulses_Per_Rotation = 1920;
+const int Pulses_Per_Rotation = 1920; //In the wiki it states there are 960 pulses, but we are checking change in states
+// i.e. rising edge and falliing, edge therefore 1920 = 960*2
 
 const int mills = 1000;
 const int delayTime = 250; 
-
-const byte Cool_Term_Min_Input_Byte_Size = 3;
 
 int rotDirection = 0;
 int speedA = 0;
@@ -33,6 +32,11 @@ int speed;
 
 void EncoderInit();
 void wheelSpeed();
+void setMotorAForward();
+void setMotorBForward();
+void setMotorAReverse();
+void setMotorBReverse();
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -50,11 +54,8 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   // Set initial rotation direction
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  Serial.println("Enter Motor Speed: ");
+  setMotorAForward();
+  setMotorBForward();
 
   EncoderInit();//Initialize the module
 
@@ -63,31 +64,60 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  analogWrite(enA, speedA);
-  analogWrite(enB, speedB);
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  //Serial.println(speedA);
-  //Serial.println(speedB);
-
   int num = Serial.available();
-  if(num > Cool_Term_Min_Input_Byte_Size){ //set to 2 to avoid false positives when using CoolTerm to communicate over the Serial Port
+  if(num > 0){ 
     Serial.print("Number of bytes sent is ");
     Serial.println(num);
-    speed = Serial.parseInt();
-    speedA = speed;
-    speedB = speed;
-    Serial.print("MotorA speed is: ");
-    Serial.println(speedA);
-    Serial.print("MotorB speed is: ");
-    Serial.println(speedB);
-    Serial.println("Enter Motor Speed: ");
+
+    int recievedVal = Serial.read();
+    Serial.print("The recieved Value is ");
+    Serial.println(recievedVal);
+
+    if(recievedVal == '0'){// Set motor speed to 0
+      speedA = 0;
+      speedB = 0;
+      analogWrite(enA, speedA);
+      analogWrite(enB, speedB);
+    }
+    else if (recievedVal == 'r'){//Place motors in reverse motion
+      setMotorAReverse();
+      setMotorBReverse();
+      Serial.print("Reverse Motors");
+    }
+    else if (recievedVal == 'f'){//place motors in forward motion
+      setMotorAForward();
+      setMotorBForward();
+      Serial.print("forward Motors");
+    }
+    else if (recievedVal >= '1' && recievedVal <= '9'){//A non-zero motor speed was entered
+      byte digits = num - 1;
+      if(digits == 1){//only one digit was entered
+        speed = recievedVal - '0';
+      }
+      else if(digits == 2){//two digits were entered
+        int value = Serial.read();
+        speed = 10*(recievedVal - '0') + value- '0';
+      }
+      else if(digits == 3){//three digits were entered
+        int value2 = Serial.read(), value3 = Serial.read();
+        speed = 100*(recievedVal - '0') + 10*(value2 -'0') +  value3- '0';
+      }
+
+      Serial.read();//to get the carriage return bit
+      
+      speedA = speed; speedB = speed;
+      analogWrite(enA, speedA);
+      analogWrite(enB, speedB);
+      Serial.print("MotorA speed is: ");
+      Serial.println(speedA);
+      Serial.print("MotorB speed is: ");
+      Serial.println(speedB);
+    }
+    
+
+
   }
-  //else{
-  //  Serial.println("There is no Serial Input");
-  //}
+  
   Serial.print("Pulse: ");
   Serial.print(duration);
 
@@ -101,27 +131,51 @@ void loop() {
 void EncoderInit()
 {
   Direction = true;//default -> Forward
-  pinMode(encA_B,INPUT);
+  pinMode(encA2,INPUT);
   attachInterrupt(0, wheelSpeed, CHANGE);
 }
 
+/**
+ * ISR.  Incrememnts/decrememnts the number of pulses for when the motor is rotating forward/backward
+ * On rising edge it verifies the direction of the motor.
+ */
 void wheelSpeed()
 {
-  int Lstate = digitalRead(encA_A);
-  if((encoder0PinALast == LOW) && Lstate==HIGH)
+  int current_A1 = digitalRead(encA1);
+  if((previous_A1 == LOW) && current_A1==HIGH)//only checking direction when A1 rises from LOW to HIGH
   {
-    int val = digitalRead(encA_B);
-    if(val == LOW && Direction)
+    int val_A2 = digitalRead(encA2);
+    if(val_A2 == LOW && Direction)//Checks if wheel is no longer moving forward and is now in reverse
     {
       Direction = false; //Reverse
     }
-    else if(val == HIGH && !Direction)
+    else if(val_A2 == HIGH && !Direction)//Checks if wheel is no longer reversing and is now moving forward
     {
       Direction = true;  //Forward
     }
   }
-  encoder0PinALast = Lstate;
+  previous_A1 = current_A1;
 
   if(!Direction)  duration++;
   else  duration--;
+}
+
+void setMotorAForward(){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+}
+
+void setMotorBForward(){
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void setMotorAReverse(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+}
+
+void setMotorBReverse(){
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
