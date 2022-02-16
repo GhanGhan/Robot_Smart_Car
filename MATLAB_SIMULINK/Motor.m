dc31b04954341202fa697e3d6defe69b18248110
GhanGhan <ghanlawal@gmail.com>
2022-02-15 23:05:43 -0500

    Show difference between simulation code on the Arduino Hardware and deploying it

--- MATLAB_SIMULINK/Motor.m
+++ MATLAB_SIMULINK/Motor.m
@@ -7,7 +7,7 @@ writeDigitalPin(a, 'D12', 0); %% Set direction of motor
 writeDigitalPin(a, 'D13', 1);
 while time > 0
     voltage = readVoltage(a, 'A0');         %% Get Voltage from potentiometer
-    fprintf('Pot value/: %f \n', voltage);
+    fprintf('Pot value: %f \n', voltage);
     writePWMVoltage(a, 'D11', voltage);     %% Write Voltage to motor
     writePWMVoltage(a, 'D6', voltage);      %% Write Voltage to LED
     time = time - 1;
@@ -20,7 +20,7 @@ writeDigitalPin(a, 'D12', 1);%% Reverse direction of motor
 writeDigitalPin(a, 'D13', 0);
 while time > 0
     voltage = readVoltage(a, 'A0');         %% Get Voltage from potentiometer
-    fprintf('Pot value/: %f \n', voltage);
+    fprintf('Pot value: %f \n', voltage);
     writePWMVoltage(a, 'D11', voltage);     %% Write Voltage to motor
     writePWMVoltage(a, 'D6', voltage);      %% Write Voltage to LED
 
