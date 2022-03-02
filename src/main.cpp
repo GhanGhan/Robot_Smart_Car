3751b5e1e64e9fb7c989be7df37ec0b2a40acd38
GhanGhan <ghanlawal@gmail.com>
2022-03-02 00:48:16 -0500

    Add applyControlInput, saturateControlInput, printFromPort, printFromBT, getSpeed_Port and getSpeed_Bt functions

--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,18 @@ const int serialPort = 53;      //checks if have signal to connect to a serial p
 
 int getNegativeSpeed_Port(int num);
 int getPositiveSpeed_Port(int num, int recievedVal);
+int getSpeed_Port(int num);
 
 int getNegativeSpeed_BT(int num);
 int getPositiveSpeed_BT(int num, int recievedVal);
+int getSpeed_BT(int num1);
+
+void printFromPort(int printErrors = 0, int printRPMs = 1, int printControlSignals = 0);
+void printFromBT(int printErrors = 0, int printRPMs = 1, int printControlSignals = 0);
+
+void saturateControlInput();
+void applyControlInput();
+
 
 void setup() {
   
@@ -78,56 +87,26 @@ void loop() {
   int num = Serial.available();
   if(num > 0  && digitalRead(serialPort)==HIGH)
   { 
-    //Serial.print("Number of bytes sent is ");
-    //Serial.println(num);
-    int recievedVal = Serial.read();
-    if(recievedVal == '0') // Set motor speed to 0
-    {
-      speed = 0;
-    }
-    else if(recievedVal == '-')//A non-zero negative motor speed
-    {
-      speed = getNegativeSpeed_Port(num);
-    }
-    else if (recievedVal >= '1' && recievedVal <= '9')//A non-zero motor positive speed was entered
-    {
-      speed = getPositiveSpeed_Port(num, recievedVal);
-    }//end recievedVal if-else-if statements
+    speed = getSpeed_Port(num);
     //r(t)
     speedA = speed; speedB = speed;
     Serial.read();
-  }// end num if statement -- SERIALPORT
+  }// end=if statement -- SERIALPORT
     
   ///---------------FOR BLUETOOTH CONNECTION!!!!!!!
-  ///*
   int num1 = Serial1.available();
   if(num1 > 0 && digitalRead(phoneBluetooth)==HIGH)
   {
-    int tempSpeed;
-    int recievedVal = Serial1.read();
-    if(recievedVal == START_BYTE)
-    {
-      recievedVal = Serial1.read();
-      num1 = num1 - 2; //'-2' for the START_BYTE and END_BYTE bytes
-      if(recievedVal == '0') // Set motor speed to 0
-      {
-        tempSpeed = 0;
-      }
-      else if(recievedVal == '-') //A non-zero negative motor speed was recieved
-      {
-        tempSpeed = getNegativeSpeed_BT(num1);
-      }
-      else if (recievedVal >= '1' && recievedVal <= '9') //A non-zero motor positive speed was recieved
-      {
-        tempSpeed = getPositiveSpeed_BT(num1, recievedVal);
-      }
-      if(Serial1.read() == 'b'){
+    if(Serial1.read() == START_BYTE){
+      int tempSpeed;
+      tempSpeed = getSpeed_BT(num1);
+      if(Serial1.read() == END_BYTE){
         speed = tempSpeed;
         speedA = speed; speedB = speed;
       }
     }
     Serial1.read();//to get the carriage return byte
-  }// end num if statement -- PHONEBLUETOOTH
+  }// end if statement -- PHONEBLUETOOTH
 
   
   //e(t)
@@ -144,7 +123,6 @@ void loop() {
     errorBIntegral = 0; errorBDerivative = 0;
   }
 
-
   //u(t)
   conA = KP*errorA + KI*errorAIntegral + KD*errorADerivative;
   conB = KP*errorB + KI*errorBIntegral + KD*errorBDerivative;
@@ -152,95 +130,22 @@ void loop() {
   oldErrorA = errorA;
   oldErrorB = errorB;
   
-  if(conA > 255)
-    conA = 255;
-  else if(conA < -255)
-    conA = -255;
-  if(conB > 255)
-    conB = 255;
-  else if(conB < -255)
-    conB = -255;
-  
-  if(conA < 0)
-    setMotorAReverse();
-  else if(conA > 0)
-    setMotorAForward();
+  // Saturate control signal if it goes above/below the largest value for analogWrite
+  saturateControlInput();
+  applyControlInput();
 
-  if(conB < 0)
-    setMotorBReverse();
-  else if(conB > 0)
-    setMotorBForward();
-
-  
-  
-  analogWrite(enA, fabs(conA));
-  analogWrite(enB, fabs(conB));
- 
   rpmA = (durationA/(float)Pulses_Per_Rotation)*60*mills/delayTime;
   rpmB = (durationB/(float)Pulses_Per_Rotation)*60*mills/delayTime;
  
   //if(digitalRead(serialPort)==HIGH) ---Commented out because I want to be able to plot speed even when connected to Phone
   //{
-    //Serial.print("Error A: ");
-    //Serial.print(errorA);
-    //Serial.print(",");
-    Serial.print("RpmA: ");
-    Serial.print(rpmA);
-    //Serial.print(",");
-    //Serial.print("Control A: ");
-    //Serial.print(conA);
-    //Serial.print(",");
-    //Serial.print("Direction A: ");
-    //Serial.print(DirectionA);
-    //Serial.print(",");
-    //Serial.print("Error B: ");
-    //Serial.print(errorB);
-    Serial.print(",");
-    Serial.print(" Rpm B: ");
-    Serial.print(rpmB);
-    Serial.print(",");
-    Serial.print(" Speed: ");
-    Serial.print(speed);
-    //Serial.print(",");
-    //Serial.print("Control B: ");
-    //Serial.print(conB);
-    //Serial.print(",");
-    //Serial.print("Direction B: ");
-    //Serial.print(DirectionB);
-    Serial.println();
+    printFromPort(0, 1, 1);
   //}
 
   if(digitalRead(phoneBluetooth)==HIGH)
   {
-    //Serial.print("Error A: ");
-    //Serial.print(errorA);
-    //Serial.print(",");
-    Serial1.print("RpmA: ");
-    Serial1.print(rpmA);
-    //Serial.print(",");
-    //Serial.print("Control A: ");
-    //Serial.print(conA);
-    //Serial.print(",");
-    //Serial.print("Direction A: ");
-    //Serial.print(DirectionA);
-    //Serial.print(",");
-    //Serial.print("Error B: ");
-    //Serial.print(errorB);
-    Serial1.print(",");
-    Serial1.print(" Rpm B: ");
-    Serial1.print(rpmB);
-    Serial1.print(",");
-    Serial1.print(" Speed: ");
-    Serial1.print(speed);
-    //Serial.print(",");
-    //Serial.print("Control B: ");
-    //Serial.print(conB);
-    //Serial.print(",");
-    //Serial.print("Direction B: ");
-    //Serial.print(DirectionB);
-    Serial1.println();
+    printFromBT(0, 1, 0);
   }
-  
   
   durationA = 0;
   durationB = 0;
@@ -251,37 +156,55 @@ void loop() {
 
 int getNegativeSpeed_Port(int num){
   byte digits = num - 2;
-  int speed;
+  int motorSpeed;
   if(digits == 1){//only one digit was entered
     int value1 = Serial.read();
-    speed = -(value1 - '0');
+    motorSpeed = -(value1 - '0');
   }
   else if(digits == 2){//two digits were entered
     int value1 = Serial.read(), value2 = Serial.read();
-    speed = -(10*(value1 - '0') + value2 - '0');
+    motorSpeed = -(10*(value1 - '0') + value2 - '0');
   }
   else if(digits == 3){//three digits were entered
     int value1 = Serial.read(), value2 = Serial.read(), value3 = Serial.read();
-    speed = -(100*(value1 - '0') + 10*(value2 -'0') +  value3 - '0');
+    motorSpeed = -(100*(value1 - '0') + 10*(value2 -'0') +  value3 - '0');
   }
-  return speed;
+  return motorSpeed;
 }
 
 int getPositiveSpeed_Port(int num, int recievedVal){
   byte digits = num - 1;
-  int speed;
+  int motorSpeed;
     if(digits == 1){//only one digit was entered
-    speed = recievedVal - '0';
+    motorSpeed = recievedVal - '0';
   }
   else if(digits == 2){//two digits were entered
     int value = Serial.read();
-    speed = 10*(recievedVal - '0') + value - '0';
+    motorSpeed = 10*(recievedVal - '0') + value - '0';
   }
   else if(digits == 3){//three digits were entered
     int value2 = Serial.read(), value3 = Serial.read();
-    speed = 100*(recievedVal - '0') + 10*(value2 -'0') +  value3- '0';
+    motorSpeed = 100*(recievedVal - '0') + 10*(value2 -'0') +  value3- '0';
   }
-  return speed;
+  return motorSpeed;
+}
+
+int getSpeed_Port(int num){
+  int recievedVal = Serial.read();
+  int motorSpeed;
+    if(recievedVal == '0') // Set motor speed to 0
+    {
+      motorSpeed = 0;
+    }
+    else if(recievedVal == '-')//A non-zero negative motor speed was entered
+    {
+      motorSpeed = getNegativeSpeed_Port(num);
+    }
+    else if (recievedVal >= '1' && recievedVal <= '9')//A non-zero motor positive speed was entered
+    {
+      motorSpeed = getPositiveSpeed_Port(num, recievedVal);
+    }//end recievedVal if-else-if statements
+    return motorSpeed;
 }
 
 int getNegativeSpeed_BT(int num){
@@ -318,3 +241,117 @@ int getPositiveSpeed_BT(int num, int recievedVal){
   }
   return speed;
 }
+
+int getSpeed_BT(int num1){
+  int motorSpeed;
+  int recievedVal = Serial1.read();
+  num1 = num1 - 2; //'-2' for the START_BYTE and END_BYTE bytes
+  if(recievedVal == '0') // Set motor speed to 0
+  {
+    motorSpeed = 0;
+  }
+  else if(recievedVal == '-') //A non-zero negative motor speed was recieved
+  {
+    motorSpeed = getNegativeSpeed_BT(num1);
+  }
+  else if (recievedVal >= '1' && recievedVal <= '9') //A non-zero motor positive speed was recieved
+  {
+    motorSpeed = getPositiveSpeed_BT(num1, recievedVal);
+  }
+  return motorSpeed;
+}
+
+
+void printFromPort(int printErrors, int printRPMs, int printControlSignals){
+  if(printErrors == 1){
+    Serial.print("Error A: ");
+    Serial.print(errorA);
+    Serial.print(",");
+    Serial.print("Error B: ");
+    Serial.print(errorB);
+    Serial.print(",");
+  }
+  
+  if(printRPMs == 1){
+    Serial.print("RpmA: ");
+    Serial.print(rpmA);
+    Serial.print(",");
+    Serial.print(" Rpm B: ");
+    Serial.print(rpmB);
+    Serial.print(",");
+  }
+  
+  if(printControlSignals == 1){
+    Serial.print("Control A: ");
+    Serial.print(conA);
+    Serial.print(",");
+    Serial.print("Control B: ");
+    Serial.print(conB);
+    Serial.print(",");
+  }
+  
+  Serial.print(" Speed: ");
+  Serial.print(speed);
+  Serial.println();
+}
+
+void printFromBT(int printErrors, int printRPMs, int printControlSignals){
+  if(printErrors == 1){
+    Serial1.print("Error A: ");
+    Serial1.print(errorA);
+    Serial1.print(",");
+    Serial1.print("Error B: ");
+    Serial1.print(errorB);
+    Serial1.print(",");
+  }
+  
+  if(printRPMs == 1){
+    Serial1.print("RpmA: ");
+    Serial1.print(rpmA);
+    Serial1.print(",");
+    Serial1.print(" Rpm B: ");
+    Serial1.print(rpmB);
+    Serial1.print(",");
+  }
+  
+  if(printControlSignals == 1){
+    Serial1.print("Control A: ");
+    Serial1.print(conA);
+    Serial1.print(",");
+    Serial1.print("Control B: ");
+    Serial1.print(conB);
+    Serial1.print(",");
+  }
+  
+  Serial1.print(" Speed: ");
+  Serial1.print(speed);
+  Serial1.println();
+}
+
+
+void saturateControlInput(){
+   if(conA > 255)
+    conA = 255;
+  else if(conA < -255)
+    conA = -255;
+  if(conB > 255)
+    conB = 255;
+  else if(conB < -255)
+    conB = -255;
+}
+
+void applyControlInput(){
+  if(conA < 0)
+    setMotorAReverse();
+  else if(conA > 0)
+    setMotorAForward();
+
+  if(conB < 0)
+    setMotorBReverse();
+  else if(conB > 0)
+    setMotorBForward();
+
+  analogWrite(enA, fabs(conA));
+  analogWrite(enB, fabs(conB));
+}
+
